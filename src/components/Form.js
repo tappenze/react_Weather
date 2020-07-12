03f6b985d90e072116217effc7984a2940682613
tappenze <42841552+tappenze@users.noreply.github.com>
2020-07-12 17:34:27 -0400

    Upload from local

---
+++ src/components/Form.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Form extends React.Component {
+  render() {
+    return <form onSubmit={this.props.getWeather}>
+        <input type="text" name="city" placeholder = "City..."/>
+        <input type="text" name="country" placeholde = "Country ..."/>
+        <button>Get Weather</button>
+    </form>;
+  }
+}
+
+
+export default Form;
